510b3ba9dec43da234e8a91dd6a2bef32be072b4
galborsz <galborsz@gmail.com>
2023-12-07 10:57:09 +0100

    my second commit

---
+++ notebooks/Internship/test.jl
@@ -0,0 +1,18 @@
+using Revise
+using DrWatson
+@quickactivate "SpikingNeuralNetworks"
+using Plots
+using SpikingNeuralNetworks
+
+default(
+    size=(800, 600),
+    tickfontsize=8,
+    guidefontsize=10,
+    margin=8Plots.mm,
+    titlefontsize=13,
+    titlefontcolor=:teal,
+    legend = true,
+    palette= (:Dark2_5),
+)
+
+SNN.@load_units 
