8af2f4acb2c47569c0d647f1e3ea7e2751c74917
Alessio <alessio.quaresima@mpi.nl>
2024-01-04 11:43:33 +0100

    run sim with debugger and added Exc and Inh reversal potentials

--- examples/AdEx_LKD.jl
+++ examples/AdEx_LKD.jl
@@ -3,7 +3,7 @@ using SpikingNeuralNetworks
 SNN.@load_units
 import SpikingNeuralNetworks: AdExParameter, IFParameter
 using Statistics, Random
-
+using Logging
 
 ## LKD parameters
 τm = 20ms
@@ -11,15 +11,15 @@ C = 300SNN.pF # Capacitance
 R = τm / C
 τre = 1ms # Rise time for excitatory synapses
 τde = 6ms # Decay time for excitatory synapses
-τri = 0.5ms # Rise time for inhibitory synapses # TODO: why does he use -?
+τri = 0.5ms # Rise time for inhibitory synapses 
 τdi = 2ms # Decay time for inhibitory synapses
 
 LKD_AdEx_exc =
     AdExParameter(τm = 20ms, Vt = -52mV, Vr = -60mV, El = -70mV, R = R, 
-    b = 0.000805nA, τw = 150ms, τre = τre, τde = τde, τri = τri, τdi = τdi, At = 10mV, τT = 30ms, E = 0mV)
+    b = 0.000805nA, τw = 150ms, τre = τre, τde = τde, τri = τri, τdi = τdi, At = 10mV, τT = 30ms, E_i=-75mV, E_e = 0mV)
 
 LKD_IF_inh =
-    IFParameter(τm = 20ms, Vt = -52mV, Vr = -60mV, El = -62mV, τre = τre, τde = τde, τri = τri, τdi = τdi, E = -75mV)
+    IFParameter(τm = 20ms, Vt = -52mV, Vr = -60mV, El = -62mV, τre = τre, τde = τde, τri = τri, τdi = τdi, E_i = -75mV, E_e=0mV)
 # LKD_AdEx_inh =
 #     AdExParameter(τm = 20ms, Vt = -52mV, Vr = -60mV, El = -62mV, R = R, 
 #     b = 0.000805nA, τw = 150ms, τre = τre, τde = τde, τri = τri, τdi = τdi, At = 10mV, τT = 30ms, E = -75mV)
@@ -55,10 +55,17 @@ ProjI = SNN.SpikingSynapse(Input_I, I, :ge; σ = σ_in_E, p = p_in)
 P = [E, I, Input_E, Input_I]
 C = [EE, EI, IE, II, ProjE, ProjI]
 
-##
-Random.seed!(23)
+#
+Random.seed!(28)
 SNN.monitor([E, I], [:fire])
-SNN.sim!(P, C; duration = 15second)
-bar(sum(hcat(E.records[:fire]...) ./ 15, dims = 2)[:, 1])
-SNN.raster([E, I], [9, 11] .* 10e3)
+
+debuglogger = ConsoleLogger(stderr, Logging.Debug)
+with_logger(debuglogger) do
+    @debug "Start the simulation"
+    SNN.sim!(P, C; duration = 1second)
+end
+##
+bar(sum(hcat(E.records[:fire]...) ./ 1, dims = 2)[:, 1])
+# SNN.raster([E, I], [9, 11] .* 10e3)
+SNN.raster([E, I], [0, 1] .* 10e3)
     
