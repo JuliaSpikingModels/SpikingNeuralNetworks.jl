7f9f1a75d37ed95f5f7ae37af8bf88923367ba18
Alessio <alessio.quaresima@mpi.nl>
2024-06-12 15:37:40 +0200

    ProgressBars

--- src/main.jl
+++ src/main.jl
@@ -1,16 +1,21 @@
-function sim!(P, C, dt)
+function sim!(P::Vector, C::Vector, dt::Float32)
+    # Threads.@threads 
     for p in P
-        integrate!(p, p.param, Float32(dt))
+        integrate!(p, getfield(p,:param), dt)
         record!(p)
     end
+    # Threads.@threads 
     for c in C
-        forward!(c, c.param)
+        forward!(c, getfield(c,:param))
         record!(c)
     end
 end
 
-function sim!(P, C; dt = 0.1ms, duration = 10ms)
-    for t = 0ms:dt:(duration - dt)
+function sim!(P, C; dt = 0.1f0, duration = 10.f0)
+    dt = Float32(dt)
+    duration= Float32(duration)
+    pbar = ProgressBar(0.f0:dt:(duration - dt))
+    for t = pbar
         sim!(P, C, dt)
     end
 end
@@ -32,3 +37,5 @@ function train!(P, C; dt = 0.1ms, duration = 10ms)
         train!(P, C, Float32(dt), Float32(t))
     end
 end
+
+export sim!, train!
