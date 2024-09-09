6ec78a41bf12b72003e8ded8892c64a3a42eb2be
Alessio <alessio.quaresima@mpi.nl>
2024-09-09 12:00:20 +0200

    spiking synapses, soma and tripod

--- src/synapse/spiking_synapses/spiking_synapse.jl
+++ src/synapse/spiking_synapses/spiking_synapse.jl
@@ -1,89 +1,103 @@
+abstract type SpikingSynapse end
 
-@snn_kw struct SynapseTripod{MFT=Matrix{Float32}, VIT=Vector{Int32},VFT=Vector{Float32},VBT=Vector{Bool}, S=Synapse, VRT= Vector{ Float32}}
-	param::SpikingSynapseParameter=no_STDPParameter()
+struct no_STDPParameter <: SpikingSynapseParameter end
+
+@snn_kw mutable struct STDP{VIT = Vector{Int32},VFT = Vector{Float32},VBT = Vector{Bool}} <:
+                       SpikingSynapse
+    param::STDPParameter = STDPParameter()
     rowptr::VIT # row pointer of sparse W
     colptr::VIT # column pointer of sparse W
     I::VIT      # postsynaptic index of W
     J::VIT      # presynaptic index of W
     index::VIT  # index mapping: W[index[i]] = Wt[i], Wt = sparse(dense(W)')
-
     W::VFT  # synaptic weight
-    v_post::VFT
-    g::MFT  # rise conductance
-	# target receptors specific alpha
-    αs::VRT
-    receptors::VIT
-	# Plasticity variables
-    tpre::VFT = zero(W)  # presynaptic spiking time
+    tpre::VFT = zero(W) # presynaptic spiking time
     tpost::VFT = zero(W) # postsynaptic spiking time
-    Apre::VFT = zero(W)  # presynaptic trace
+    Apre::VFT = zero(W) # presynaptic trace
     Apost::VFT = zero(W) # postsynaptic trace
     fireI::VBT # postsynaptic firing
     fireJ::VBT # presynaptic firing
+    g::VFT # postsynaptic conductance
     records::Dict = Dict()
 end
 
-"""
-[Spiking Synapse](https://brian2.readthedocs.io/en/2.0b4/resources/tutorials/2-intro-to-brian-synapses.html)
-"""
-function SynapseTripod(pre, post, target::String, type::String; w = nothing, σ = -1.0, p = 0.0, kwargs...)
-	# Get the parameters for post-synaptic cell
-	@unpack dend_syn = post
-	@unpack soma_syn = post
-	# Create the sparse matrix
-	if w === nothing
-        w = σ * sprand(post.N, pre.N, p)
-    else
-        w = sparse(w)
-    end
-    rowptr, colptr, I, J, index, W = dsparse(w)
-    fireI, fireJ = post.fire, pre.fire
-
-	# Get the target conductance (pointer to array)
-    g = getfield(post, Symbol("h_$target"))
-    v_post = getfield(post, Symbol("v_$target"))
-	if target =="s"
-		if type =="exc"
-			receptors = [1]
-		elseif type =="inh"
-			receptors = [2]
-		end
-        αs = [post.soma_syn[i].α for i in eachindex(receptors)]
-	else
-		# param = deepcopy(dend_syn)
-		if type =="exc"
-			receptors = [1,2]
-		elseif type =="inh"
-			receptors = [3,4]
-		end
-        αs = [post.dend_syn[i].α for i in eachindex(receptors)]
-	end
-    SynapseTripod(;@symdict(rowptr, colptr, I, J, index, receptors, W,  g,  αs, v_post, fireI, fireJ)..., kwargs...)
+@snn_kw mutable struct vSTDP{
+    VIT = Vector{Int32},
+    VFT = Vector{Float32},
+    VBT = Vector{Bool},
+} <: SpikingSynapse
+    param::vSTDPParameter = vSTDPParameter()
+    rowptr::VIT # row pointer of sparse W
+    colptr::VIT # column pointer of sparse W
+    I::VIT      # postsynaptic index of W
+    J::VIT      # presynaptic index of W
+    index::VIT  # index mapping: W[index[i]] = Wt[i], Wt = sparse(dense(W)')
+    W::VFT  # synaptic weight
+    W0::VFT = deepcopy(W)
+    u::VFT = zeros(length(colptr) - 1) # presynaptic spiking time
+    v::VFT = zeros(length(colptr) - 1) # postsynaptic spiking time
+    x::VFT = zeros(length(colptr) - 1) # postsynaptic spiking time
+    vpost::VFT = zeros(length(colptr) - 1)
+    fireJ::VBT # presynaptic firing
+    g::VFT # postsynaptic conductance
+    records::Dict = Dict()
 end
 
-function forward!(c::SynapseTripod, param::SpikingSynapseParameter)
-    @unpack colptr, I, W, fireJ,  g = c
-    @unpack receptors, αs = c
-    @fastmath for r in eachindex(receptors) 
-         @inbounds @simd for j in 1:(length(colptr) - 1)
-            if fireJ[j]
-                for s in colptr[j]:(colptr[j+1] - 1)
-                    g[I[s],receptors[r]] +=  W[s]*αs[r]
-                end
-            end
-        end
-	end
+@snn_kw mutable struct iSTDP{
+    VIT = Vector{Int32},
+    VFT = Vector{Float32},
+    VBT = Vector{Bool},
+} <: SpikingSynapse
+    param::iSTDPParameter = iSTDPParameterRate()
+    rowptr::VIT # row pointer of sparse W
+    colptr::VIT # column pointer of sparse W
+    I::VIT      # postsynaptic index of W
+    J::VIT      # presynaptic index of W
+    index::VIT  # index mapping: W[index[i]] = Wt[i], Wt = sparse(dense(W)')
+    W::VFT  # synaptic weight
+    tpost::VFT = zeros(length(rowptr) - 1) # postsynaptic spiking time 
+    tpre::VFT = zeros(length(colptr) - 1) # presynaptic spiking time
+    fireI::VBT
+    fireJ::VBT # presynaptic firing
+    g::VFT # postsynaptic conductance
+    records::Dict = Dict()
 end
 
+@snn_kw mutable struct no_STDP{
+    VIT = Vector{Int32},
+    VFT = Vector{Float32},
+    VBT = Vector{Bool},
+} <: SpikingSynapse
+    param::SpikingSynapseParameter = no_STDPParameter()
+    rowptr::VIT # row pointer of sparse W
+    colptr::VIT # column pointer of sparse W
+    I::VIT      # postsynaptic index of W
+    J::VIT      # presynaptic index of W
+    index::VIT  # index mapping: W[index[i]] = Wt[i], Wt = sparse(dense(W)')
+    W::VFT  # synaptic weight
+    fireJ::VBT # presynaptic firing
+    g::VFT # postsynaptic conductance
+    records::Dict = Dict()
+end
+# function forward!(c::InhSynapseTripod, param::SpikingSynapseParameter)
+#     @unpack colptr, I, W, fireJ, g, αs = c
+#     @inbounds @simd for j in 1:(length(colptr)-1)
+#         if fireJ[j]
+#             for s in colptr[j]:(colptr[j+1]-1)
+#                 g[I[s], :] .+= W[s] .* αs
+#             end
+#         end
+#     end
+# end
 
 """
 [Spking Synapse](https://brian2.readthedocs.io/en/2.0b4/resources/tutorials/2-intro-to-brian-synapses.html)
 """
-SpikingSynapse 
+SpikingSynapse
 
 function SpikingSynapse(pre, post, sym; σ = 0.0, p = 0.0, kwargs...)
     w = σ * sprand(post.N, pre.N, p) # Construct a random sparse vector with length post.N, pre.N and density p
-    w[findall(w.>0)] .= σ # make all the weights the same
+    w[findall(w .> 0)] .= σ # make all the weights the same
     rowptr, colptr, I, J, index, W = dsparse(w) # Get info about the existing connections
     # rowptr: row pointer
     # colptr: column pointer
@@ -95,12 +109,12 @@ function SpikingSynapse(pre, post, sym; σ = 0.0, p = 0.0, kwargs...)
     g = getfield(post, sym)
     # g: get variable with symbol sym
     param = haskey(kwargs, :param) ? kwargs[:param] : nothing
-    if isa(param, STDPParameter) 
+    if isa(param, STDPParameter)
         return STDP(;
             @symdict(rowptr, colptr, I, J, index, W, fireI, fireJ, g)...,
             kwargs...,
         )
-    elseif isa(param, vSTDPParameter) 
+    elseif isa(param, vSTDPParameter)
         return vSTDP(;
             @symdict(rowptr, colptr, I, J, index, W, fireJ, vpost, g)...,
             kwargs...,
@@ -111,14 +125,11 @@ function SpikingSynapse(pre, post, sym; σ = 0.0, p = 0.0, kwargs...)
             kwargs...,
         )
     else
-        return no_STDP(;
-            @symdict(rowptr, colptr, I, J, index, W, g, fireJ)...,
-            kwargs...,
-        )
+        return no_STDP(; @symdict(rowptr, colptr, I, J, index, W, g, fireJ)..., kwargs...)
     end
 end
 
-function SpikingSynapse(;kwargs...)
+function SpikingSynapse(; kwargs...)
     isa(kwargs.param, STDPParameter) && STDP(kwargs...)
     isa(kwargs.param, vSTDPParameter) && vSTDP(kwargs...)
     isa(kwargs.param, iSTDPParameter) && iSTDP(kwargs...)
@@ -137,4 +148,4 @@ function forward!(c::SpikingSynapse, param::SpikingSynapseParameter)
     end
 end
 
-export SynapseTripod, SpikingSynapse
+export SpikingSynapse
