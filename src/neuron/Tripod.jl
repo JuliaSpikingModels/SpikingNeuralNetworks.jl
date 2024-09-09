f6f139a82d0f63fa9170e0d6060c03003fb9ec2e
Alessio <alessio.quaresima@mpi.nl>
2024-09-09 12:04:37 +0200

    Tripod neuron implementation

--- src/neuron/Tripod.jl
+++ src/neuron/Tripod.jl
@@ -1,7 +1,7 @@
 # Get synapses from SNNUtils
 
 
-@snn_kw struct AdExTripod{FT <: Float32} <: AbstractIFParameter
+@snn_kw struct AdExTripod{FT = Float32,IT = Int64} <: AbstractIFParameter
     #Membrane parameters
     C::FT = 281pF           # (pF) membrane timescale
     gl::FT = 40nS                # (nS) gl is the leaking conductance,opposite of Rm
@@ -17,20 +17,20 @@
     a::FT = 4nS            #nS adaptation current to membrane
     b::FT = 80.5pA         #pA adaptation current increase due to spike
     # After spike timescales and membrane
-    AP_membrane::Float32 = 10.0f0mV
-    BAP::Float32 = 1.0f0mV
-    up::Int64 = 1ms
-    idle::Int64 = 2ms
+    AP_membrane::FT = 10.0f0mV
+    BAP::FT = 1.0f0mV
+    up::IT = 1ms
+    idle::IT = 2ms
 end
 
 
-@snn_kw struct PostSpike{FT <:Float32}
+@snn_kw struct PostSpike{FT<:Float32}
     A::FT
     τA::FT
 end
 
 
-@snn_kw struct Dendrite{FT =  Float32}
+@snn_kw struct Dendrite{FT = Float32}
     Er::FT = -70.6mV             # (mV) resting potential
     C::FT = 10pF                 # (1/pF) membrane timescale
     gax::FT = 10nS# (nS) axial conductance
@@ -39,98 +39,172 @@ end
     d::FT = 4um# μm dendrite diameter
 end
 
-
-@snn_kw struct Tripod{MFT = Matrix{Float32},VIT = Vector{Int32},VFT = Vector{Float32},VBT = Vector{Bool}, VDT=Vector{Dendrite}, ST=SynapseArray, NMDAT=NMDAVoltageDependency, PST=PostSpike} <: AbstractIF
-    param::AdExTripod = AdExTripod()
+@snn_kw struct Tripod{
+    MFT = Matrix{Float32},
+    VIT = Vector{Int32},
+    VFT = Vector{Float32},
+    VBT = Vector{Bool},
+    VDT = Vector{Dendrite{Float32}},
+    ST = SynapseArray,
+    NMDAT = NMDAVoltageDependency{Float32},
+    PST = PostSpike{Float32},
+    IT = Int32,
+    FT = Float32,
+    AdExType = AdExTripod,
+} <: AbstractIF
+    t::VIT = [0]
+    param::AdExType = AdExTripod()
     ## These are compulsory parameters
-    N::Int32 = 100
-    NMDA::NMDAT =  NMDAVoltageDependency(mg=Mg_mM, b=nmda_b, k=nmda_k)
-    soma_syn::SynapseArray
-    dend_syn::SynapseArray
+    N::IT = 100
+    soma_syn::ST
+    dend_syn::ST
+    d1::VDT
+    d2::VDT
+    NMDA::NMDAT = NMDAVoltageDependency(mg = Mg_mM, b = nmda_b, k = nmda_k)
     ##
     # dendrites
-    gax1::VFT=zeros(N)
-    gax2::VFT=zeros(N)
-    cd1::VFT=zeros(N)
-    cd2::VFT=zeros(N)
-    gm1::VFT=zeros(N)
-    gm2::VFT=zeros(N)
-    # Synapses
-    τr::SArray{Tuple{4,2},Float32}
-    τd::SArray{Tuple{4,2},Float32}
-    gsyn::SArray{Tuple{4,2},Float32}
-    E_rev::SArray{Tuple{4,2},Float32}
-    rtypes::SArray{Tuple{4,2},String}
+    gax1::VFT = zeros(N)
+    gax2::VFT = zeros(N)
+    cd1::VFT = zeros(N)
+    cd2::VFT = zeros(N)
+    gm1::VFT = zeros(N)
+    gm2::VFT = zeros(N)
     ##
     # Membrane potential and adaptation
     v_s::VFT = param.Vr .+ rand(N) .* (param.Vt - param.Vr)
     w_s::VFT = zeros(N)
     v_d1::VFT = param.Vr .+ rand(N) .* (param.Vt - param.Vr)
     v_d2::VFT = param.Vr .+ rand(N) .* (param.Vt - param.Vr)
-    ##
-    g_s::MFT = zeros(N, 4)
+    # Synapses
+    g_s::MFT = zeros(N, 2)
     g_d1::MFT = zeros(N, 4)
     g_d2::MFT = zeros(N, 4)
-    h_s::MFT = zeros(N, 4)
+    h_s::MFT = zeros(N, 2)
     h_d1::MFT = zeros(N, 4)
     h_d2::MFT = zeros(N, 4)
     # Spike model and threshold
     fire::VBT = zeros(Bool, N)
     after_spike::VFT = zeros(Int, N)
-    postspike::PST = PostSpike(A=10, τA=30ms)
+    postspike::PST = PostSpike(A = 10, τA = 30ms)
     θ::VFT = ones(N) * param.Vt
     records::Dict = Dict()
-    Δv::MArray{Tuple{3},Float32}=MArray{Tuple{3}}([0.f0, 0.f0, 0.f0])
-    Δv_temp::MArray{Tuple{3},Float32}=MArray{Tuple{3}}([0.f0, 0.f0, 0.f0])
-    cs::MArray{Tuple{2},Float32}=MArray{Tuple{2}}([0.f0, 0.f0])
-    is::MArray{Tuple{3},Float32}=MArray{Tuple{3}}([0.f0, 0.f0, 0.f0])
+    Δv::VFT = zeros(3)
+    Δv_temp::VFT = zeros(3)
+    cs::VFT = zeros(2)
+    is::VFT = zeros(3)
 end
 
-function TripodPopulation(;N::Int, d1::Vector{Dendrite}, d2::Vector{Dendrite}, soma_syn::Synapse, dend_syn::Synapse, NMDA::NMDAVoltageDependency)::Tripod
+"""
+This is a struct representing a spiking neural network model that include two dendrites and a soma based on the adaptive exponential integrate-and-fire model (AdEx)
+
+# Fields 
+
+- `param::AdExTripod` : Parameters for the AdEx model.
+- `N::Int32` : The number of neurons in the network.
+- `soma_syn::ST` : Synapses connected to the soma.
+- `dend_syn::ST` : Synapses connected to the dendrites.
+- `d1::VDT`, `d2::VDT` : Dendrite structures.
+- `NMDA::NMDAT` : Specifies the properties of NMDA (N-methyl-D-aspartate) receptors.
+- `gax1::VFT`, `gax2::VFT` : Axial conductance (reciprocal of axial resistance) for dendrite 1 and 2 respectively.
+- `cd1::VFT`, `cd2::VFT` : Capacitance for dendrite 1 and 2.
+- `gm1::VFT`, `gm2::VFT` : Membrane conductance for dendrite 1 and 2.
+- `v_s::VFT` : Somatic membrane potential.
+- `w_s::VFT` : Adaptation variables for each soma.
+- `v_d1::VFT` , `v_d2::VFT` : Dendritic membrane potential for dendrite 1 and 2.
+- `g_s::MFT` , `g_d1::MFT`, `g_d2::MFT` : Conductance of somatic and dendritic synapses.
+- `h_s::MFT`, `h_d1::MFT`, `h_d2::MFT` : Synaptic gating variables.
+- `fire::VBT` : Boolean array indicating which neurons have fired.
+- `after_spike::VFT` : Post-spike timing.
+- `postspike::PST` : Model for post-spike behavior.
+- `θ::VFT` : Individual neuron firing thresholds.
+- `records::Dict` : A dictionary to store simulation results.
+- `Δv::VFT` , `Δv_temp::VFT` : Variables to hold temporary voltage changes.
+- `cs::VFT` , `is::VFT` : Temporary variables for currents.
+"""
+Tripod
+
+function TripodNeurons(;
+    N::Int,
+    d1::Vector{Dendrite{Float32}},
+    d2::Vector{Dendrite{Float32}},
+    soma_syn::Synapse,
+    dend_syn::Synapse,
+    NMDA::NMDAVoltageDependency,
+    param = AdExTripod(),
+)::Tripod
     gax1 = [d.gax for d in d1]
     gax2 = [d.gax for d in d2]
     cd1 = [d.C for d in d1]
     cd2 = [d.C for d in d2]
     gm1 = [d.gm for d in d1]
     gm2 = [d.gm for d in d2]
-    dend_syn = SNN.synapsearray(dend_syn)
-    soma_syn = SNN.synapsearray(soma_syn)
-    τr = SNN.SArray{Tuple{4,2}}(hcat([[1/r.τr for r in dend_syn], [1/r.τr for r in soma_syn]]...))
-    τd = SNN.SArray{Tuple{4,2}}(hcat([[1/r.τd for r in dend_syn], [1/r.τd for r in soma_syn]]...))
-    gsyn = SNN.SArray{Tuple{4,2}}(hcat([[r.gsyn for r in dend_syn], [r.gsyn for r in soma_syn]]...))
-    E_rev = SNN.SArray{Tuple{4,2}}(hcat([[r.E_rev for r in dend_syn], [r.E_rev for r in soma_syn]]...))
-    rtypes = SNN.SArray{Tuple{4,2}}(hcat([[r.type for r in dend_syn], [r.type for r in soma_syn]]...))
-    return Tripod(N=N, τr=τr, τd=τd, gsyn=gsyn, rtypes=rtypes, E_rev=E_rev, NMDA=NMDA, gax1=gax1, gax2=gax2, cd1=cd1, cd2=cd2, gm1=gm1, gm2=gm2, soma_syn=soma_syn, dend_syn=dend_syn)
+    return Tripod(
+        N = N,
+        d1 = d1,
+        d2 = d2,
+        soma_syn = synapsearray(soma_syn),
+        dend_syn = synapsearray(dend_syn),
+        NMDA = NMDA,
+        gax1 = gax1,
+        gax2 = gax2,
+        cd1 = cd1,
+        cd2 = cd2,
+        gm1 = gm1,
+        gm2 = gm2,
+        param = param,
+    )
 end
 
 #const dend_receptors::SVector{Symbol,3} = [:AMPA, :NMDA, :GABAa, :GABAb]
 # const soma_receptors::Vector{Symbol} = [:AMPA, :GABAa]
-const soma_rr = SA[:AMPA,:GABAa]
-const dend_rr = SA[:AMPA,:NMDA,:GABAa,:GABAb]
+const soma_rr = SA[:AMPA, :GABAa]
+const dend_rr = SA[:AMPA, :NMDA, :GABAa, :GABAb]
 
 function integrate!(p::Tripod, param::AdExTripod, dt::Float32)
-    @unpack N, v_s, w_s, v_d1, v_d2, g_s, g_d1, g_d2, h_s, h_d1, h_d2, fire, θ, after_spike, postspike, Δv, Δv_temp = p
+    @unpack N,
+    v_s,
+    w_s,
+    v_d1,
+    v_d2,
+    g_s,
+    g_d1,
+    g_d2,
+    h_s,
+    h_d1,
+    h_d2,
+    d1,
+    d2,
+    fire,
+    θ,
+    after_spike,
+    postspike,
+    Δv,
+    Δv_temp = p
     @unpack Er, up, idle, BAP, AP_membrane, Vr, Vt, τw, a, b = param
+    @unpack dend_syn, soma_syn = p
     @unpack gax1, gax2, gm1, gm2, cd1, cd2 = p
-    @unpack τr, τd = p
 
-    ## Update all synaptic conductance
-    # for dendrites
-    @inbounds for n in axes(τr,1)
+    # Update all synaptic conductance
+    for n in eachindex(dend_syn)
+        @unpack τr⁻, τd⁻ = dend_syn[n]
         @fastmath @simd for i = 1:N
-                # dendrites x = 1
-                g_d1[i, n] = exp32(-dt * τr[n,1]) * (g_d1[i, n] + dt * h_d1[i, n])
-                h_d1[i, n] = exp32(-dt * τr[n,1]) * (h_d1[i, n])
-                g_d2[i, n] = exp32(-dt * τd[n,1]) * (g_d2[i, n] + dt * h_d2[i, n])
-                h_d2[i, n] = exp32(-dt * τr[n,1]) * (h_d2[i, n])
-                #soma x = 2
-                g_s[i, n] =  exp32(-dt * τd[n,2]) * (g_s[i, n] + dt * h_s[i, n])
-                h_s[i, n] =  exp32(-dt * τr[n,2]) * (h_s[i, n])
+            g_d1[i, n] = exp32(-dt * τd⁻) * (g_d1[i, n] + dt * h_d1[i, n])
+            h_d1[i, n] = exp32(-dt * τr⁻) * (h_d1[i, n])
+            g_d2[i, n] = exp32(-dt * τd⁻) * (g_d2[i, n] + dt * h_d2[i, n])
+            h_d2[i, n] = exp32(-dt * τr⁻) * (h_d2[i, n])
+        end
+    end
+    # for soma
+    for n in eachindex(soma_syn)
+        @unpack τr⁻, τd⁻ = soma_syn[n]
+        @fastmath @simd for i = 1:N
+            g_s[i, n] = exp32(-dt * τd⁻) * (g_s[i, n] + dt * h_s[i, n])
+            h_s[i, n] = exp32(-dt * τr⁻) * (h_s[i, n])
         end
     end
 
     # update the neurons
-    @inbounds @fastmath @simd for i = 1:N
+    @inbounds for i = 1:N
         if after_spike[i] > idle
             v_s[i] = BAP
             ## backpropagation effect
@@ -148,99 +222,118 @@ function integrate!(p::Tripod, param::AdExTripod, dt::Float32)
             v_d2[i] += dt * c2 / cd2[i]
         else
             ## Heun integration
-            for _i in 1:3
-                Δv_temp[_i] = 0.f0
-                Δv[_i] = 0.f0
+            for _i = 1:3
+                Δv_temp[_i] = 0.0f0
+                Δv[_i] = 0.0f0
             end
             update_tripod!(p, Δv, i, param, 0.0f0)
-            for _i in 1:3
+            for _i = 1:3
                 Δv_temp[_i] = Δv[_i]
             end
             update_tripod!(p, Δv, i, param, dt)
-            v_s[i]  += 0.5 * dt * (Δv_temp[1] +Δv[1])
-            v_d1[i] += 0.5 * dt * (Δv_temp[2] +Δv[2])
-            v_d2[i] += 0.5 * dt * (Δv_temp[3] +Δv[3])
+            v_s[i] += 0.5 * dt * (Δv_temp[1] + Δv[1])
+            v_d1[i] += 0.5 * dt * (Δv_temp[2] + Δv[2])
+            v_d2[i] += 0.5 * dt * (Δv_temp[3] + Δv[3])
             w_s[i] += dt * ΔwAdEx(v_s[i], w_s[i], param)
         end
     end
 
     # reset firing
     fire .= false
-    @inbounds @fastmath @simd for i = 1:N
-        θ[i] -= dt * (θ[i] - Vt) / getfield(postspike,:τA)
+    @inbounds for i = 1:N
+        θ[i] -= dt * (θ[i] - Vt) / postspike.τA
         after_spike[i] -= 1
         if after_spike[i] < 0
             ## spike ?
-            if v_s[i] > θ[i] + 10.f0
+            if v_s[i] > θ[i] + 10.0f0
                 fire[i] = true
-                θ[i] += getfield(postspike,:A)
+                θ[i] += postspike.A
                 v_s[i] = AP_membrane
                 w_s[i] += b ##  *τw
                 after_spike[i] = (up + idle) / dt
             end
         end
     end
-    return 
+    return
 end
 
-function update_tripod!(p::Tripod, Δv::MVector, i::Int64, param::AdExTripod, dt::Float32)
+function update_tripod!(
+    p::Tripod,
+    Δv::Vector{Float32},
+    i::Int64,
+    param::AdExTripod,
+    dt::Float32,
+)
 
-    @inbounds @fastmath begin
+    @fastmath @inbounds begin
+        @unpack v_d1, v_d2, v_s, w_s, g_s, g_d1, g_d2, θ = p
+        @unpack gax1, gax2, gm1, gm2, cd1, cd2 = p
+        @unpack d1, d2 = p
 
-    @unpack v_d1, v_d2, v_s, w_s, g_s, g_d1, g_d2, θ = p
-    @unpack gax1, gax2, gm1, gm2, cd1, cd2 = p
-    # @unpack soma_syn, dend_syn, NMDA= p
-    @unpack gsyn, E_rev, rtypes, NMDA = p
-    @unpack is, cs = p
+        @unpack soma_syn, dend_syn, NMDA = p
+        @unpack is, cs = p
+        @unpack mg, b, k = NMDA
 
-    #compute axial currents
-    cs[1] = -( (v_d1[i] + Δv[2] * dt) - (v_s[i] + Δv[1] * dt)) * gax1[i]
-    cs[2] = -( (v_d2[i] + Δv[3] * dt) - (v_s[i] + Δv[1] * dt)) * gax2[i]
+        #compute axial currents
+        cs[1] = -((v_d1[i] + Δv[2] * dt) - (v_s[i] + Δv[1] * dt)) * gax1[i]
+        cs[2] = -((v_d2[i] + Δv[3] * dt) - (v_s[i] + Δv[1] * dt)) * gax2[i]
 
-    @simd for _i in 1:3
-        is[_i] = 0.f0
-    end
-    @simd for r in axes(gsyn,1)
-        if rtypes[r,1] =="nmda"
-            is[2] += gsyn[r,1] * g_d1[i, r]*(v_d1[i] + Δv[2] * dt - E_rev[r,1]) *(1.f0+(NMDA.mg/NMDA.b)*exp32(NMDA.k*(v_d1[i] + Δv[2] * dt)))^-1
-            is[3] += gsyn[r,1] * g_d2[i, r]*(v_d2[i] + Δv[3] * dt - E_rev[r,1]) *(1.f0+(NMDA.mg/NMDA.b)*exp32(NMDA.k*(v_d2[i] + Δv[3] * dt)))^-1
-            is[1] += gsyn[r,2] * g_s[i, r]*(v_s[i] + Δv[1] * dt - E_rev[r,2]) 
-        else
-            is[2] += gsyn[r,1] * g_d1[i, r]*(v_d1[i] + Δv[2] * dt - E_rev[r,1])
-            is[3] += gsyn[r,1] * g_d2[i, r]*(v_d2[i] + Δv[3] * dt - E_rev[r,1])
-            # (gsyn[n,2]==0) && (continue)
-            is[1] += gsyn[r,2] *  g_s[i, r]*(v_s[i] +  Δv[1] * dt - E_rev[r,2]) 
+        for _i = 1:3
+            is[_i] = 0.0f0
         end
-    end
-    @simd for _i in 1:3
-        is[_i] = clamp(is[_i], -500,500)
-    end
+        for r in eachindex(soma_syn)
+            @unpack gsyn, E_rev = soma_syn[r]
+            is[1] += gsyn * g_s[i, r] * (v_s[i] + Δv[1] * dt - E_rev)
+        end
+        for r in eachindex(dend_syn)
+            @unpack gsyn, E_rev, nmda = dend_syn[r]
+            is[2] +=
+                gsyn *
+                g_d1[i, r] *
+                (v_d1[i] + Δv[2] * dt - E_rev) *
+                ((1.0f0 + (mg / b) * exp32(k * (v_d1[i] + Δv[2] * dt)))^-1)^nmda
+            is[3] +=
+                gsyn *
+                g_d2[i, r] *
+                (v_d2[i] + Δv[3] * dt - E_rev) *
+                ((1.0f0 + (mg / b) * exp32(k * (v_d2[i] + Δv[3] * dt)))^-1)^nmda
+        end
+        for _i = 1:3
+            is[_i] = clamp(is[_i], -1500, 1500)
+        end
+        # @info Δv
+        # @info v_d1[i], Δv[2], gm1[i], is[2], cs[1], cd1[i] 
 
-    # update membrane potential
-    Δv[1] = ΔvAdEx(v_s[i] + Δv[1] * dt, w_s[i], θ[i], + sum(cs), is[1], param)
-    Δv[2] = ((-(v_d1[i] + Δv[2] * dt) + param.Er) * gm1[i] - is[2] + cs[1]) / cd1[i] 
-    Δv[3] = ((-(v_d2[i] + Δv[3] * dt) + param.Er) * gm2[i] - is[3] + cs[2]) / cd2[i] 
+        # update membrane potential
+        @unpack C, gl, Er, ΔT = param
+        Δv[1] =
+            (
+                gl * (
+                    (-v_s[i] + Δv[1] * dt + Er) +
+                    ΔT * exp32(1 / ΔT * (v_s[i] + Δv[1] * dt - θ[i]))
+                ) - w_s[i] - is[1] - sum(cs)
+            ) / C
+        Δv[2] = ((-(v_d1[i] + Δv[2] * dt) + Er) * gm1[i] - is[2] + cs[1]) / cd1[i]
+        Δv[3] = ((-(v_d2[i] + Δv[3] * dt) + Er) * gm2[i] - is[3] + cs[2]) / cd2[i]
     end
 
 end
 
 
-@inline
-@fastmath function ΔvAdEx(v::Float32, w::Float32, θ::Float32, axial::Float32, synaptic::Float32, AdEx::AdExTripod)::Float32
-    return 1/ AdEx.C * (
-        AdEx.gl * (
-                (-v + AdEx.Er) + 
-                AdEx.ΔT * exp32(1 / AdEx.ΔT * (v - θ))
-                ) 
-                - w 
-                - synaptic 
-                - axial
-        ) 
-end ## external currents
+# @inline @fastmath function ΔvAdEx(v::Float32, w::Float32, θ::Float32, axial::Float32, synaptic::Float32, AdEx::AdExTripod)::Float32
+#     return 1/ AdEx.C * (
+#         AdEx.gl * (
+#                 (-v + AdEx.Er) + 
+#                 AdEx.ΔT * exp32(1 / AdEx.ΔT * (v - θ))
+#                 ) 
+#                 - w 
+#                 - synaptic 
+#                 - axial
+#         ) 
+# end ## external currents
 
-@inline
-@fastmath function ΔwAdEx(v::Float32, w::Float32, AdEx::AdExTripod)::Float32
+@inline @fastmath function ΔwAdEx(v::Float32, w::Float32, AdEx::AdExTripod)::Float32
     return (AdEx.a * (v - AdEx.Er) - w) / AdEx.τw
 end
 
-export  Tripod, TripodPopulation, Dendrite, PostSpike
+export Tripod, TripodPopulation, Dendrite, PostSpike
