d8068df3ff1c9acc4c1d896f8e4d337e228944ef
Alessio <alessio.quaresima@mpi.nl>
2024-09-11 14:19:42 +0200

    added normalization to plasticity rules

---
+++ src/synapse/spiking_synapses/normalization.jl
@@ -0,0 +1,66 @@
+"""
+    Abstract type for normalization parameters.
+"""
+abstract type NormParam end
+
+"""
+    MultiplicativeNorm{FT = Int32} <: NormParam
+
+This struct holds the parameters for multiplicative normalization. 
+It includes a timescale τ (default 0.0) and an operator (default multiplication).
+"""
+@snn_kw struct MultiplicativeNorm{FT = Int32} <: NormParam
+    τ::Float32 = 0.0f0
+    operator::Function = *
+end
+
+"""
+    AdditiveNorm{FT = Float32} <: NormParam
+
+This struct holds the parameters for additive normalization. 
+It includes a timescale τ (default 0.0) and an operator (default addition).
+"""
+@snn_kw struct AdditiveNorm{FT = Float32} <: NormParam
+    τ::Float32 = 0.0f0
+    operator::Function = +
+end
+
+"""
+    SynapseNormalization{VFT = Vector{Float32}, VIT = Vector{Int32}, MFT = Matrix{Float32}}
+
+A struct that holds parameters for synapse normalization, including:
+- param: Normalization parameter, can be either MultiplicativeNorm or AdditiveNorm.
+- t: A vector of integer values representing time points.
+- W0: A vector of initial weights before simulation.
+- W1: A vector of weights during the simulation.
+- μ: A vector of mean synaptic weights.
+- records: A dictionary for storing additional data.
+"""
+@snn_kw struct SynapseNormalization{
+    VFT = Vector{Float32},
+    VIT = Vector{Int32},
+    MFT = Matrix{Float32},
+}
+    param::NormParam = MultiplicativeNorm()
+    t::VIT = [0, 1]
+    W0::VFT = [0.0f0]
+    W1::VFT = [0.0f0]
+    μ::VFT = [0.0f0]
+    records::Dict = Dict()
+end
+
+"""
+    SynapseNormalization(N; param, kwargs...)
+
+Constructor function for the SynapseNormalization struct.
+- N: The number of synapses.
+- param: Normalization parameter, can be either MultiplicativeNorm or AdditiveNorm.
+- kwargs: Other optional parameters.
+Returns a SynapseNormalization object with the specified parameters.
+"""
+function SynapseNormalization(N; param, kwargs...)
+    W0 = zeros(Float32, N)
+    W1 = zeros(Float32, N)
+    μ = zeros(Float32, N)
+    SynapseNormalization(; @symdict(param, W0, W1, μ)..., kwargs...)
+end
--- src/synapse/spiking_synapses/plasticity_rules.jl
+++ src/synapse/spiking_synapses/plasticity_rules.jl
@@ -3,7 +3,7 @@ function plasticity!(c::SpikingSynapse, param::no_STDPParameter, dt::Float32) en
 """
     plasticity!(c::SpikingSynapse, param::iSTDPParameterRate, dt::Float32)
 
-Performs the synaptic plasticity calculation based on the spike-timing dependent plasticity (STDP) model for a spiking neuron network. 
+Performs the synaptic plasticity calculation based on the inihibitory spike-timing dependent plasticity (iSTDP) model from Vogels (2011). 
 The function updates synaptic weights `W` of each synapse in the network according to the firing status of pre and post-synaptic neurons.
 This is an in-place operation that modifies the input `SpikingSynapse` object `c`.
 
@@ -49,7 +49,24 @@ function plasticity!(c::SpikingSynapse, param::iSTDPParameterRate, dt::Float32)
     end
 end
 
-##
+"""
+    plasticity!(c::SpikingSynapse, param::iSTDPParameterRate, dt::Float32)
+
+Performs the synaptic plasticity calculation based on the inihibitory spike-timing dependent plasticity (iSTDP) model from Vogels (2011) adapted to control the membrane potential. 
+The function updates synaptic weights `W` of each synapse in the network according to the firing status of pre and post-synaptic neurons.
+This is an in-place operation that modifies the input `SpikingSynapse` object `c`.
+
+# Arguments
+- `c::SpikingSynapse`: The current spiking synapse object which contains data structures to represent the synapse network.
+- `param::iSTDPParameterVoltage`: Parameters needed for the iSTDP model, including learning rate `η`, target membrane potenital `v0`, STDP time constant `τy`, maximal and minimal synaptic weight (`Wmax` and `Wmin`).
+- `dt::Float32`: The time step for the numerical integration.
+
+# Algorithm
+- For each pre-synaptic neuron, if it fires, it reduces the synaptic weight by an amount proportional to the difference between the target membrane potential and the actual one. 
+- For each pre-synaptic neuron, if it fires, increases the inhibitory term, otherwise the inhibitory term decays exponentially over time with a time constant `τy`.
+- For each post-synaptic neuron, if it fires, it increases the synaptic weight by an amount proportional to the pre-synaptic trace and increases the excitatory term, otherwise the excitatory term decays exponentially over time with a time constant `τy`.
+- The synaptic weights are bounded by `Wmin` and `Wmax`.
+"""
 function plasticity!(c::SpikingSynapse, param::iSTDPParameterPotential, dt::Float32)
     @unpack rowptr, colptr, index, I, J, W, v_post, tpost, tpre, fireI, fireJ, g = c
     @unpack η, v0, τy, Wmax, Wmin = param
@@ -113,11 +130,34 @@ end
 #     end
 # end
 
+"""
+    plasticity!(c::SpikingSynapse, param::vSTDPParameter, dt::Float32)
 
+Perform update of synapses using plasticity rules based on the Spike Timing Dependent Plasticity (STDP) model.
+This function updates pre-synaptic spike traces and post-synaptic membrane traces, and modifies synaptic weights using vSTDP rules.
+
+# Arguments
+- `c::SpikingSynapse`: The spiking synapse to be updated.
+- `param::vSTDPParameter`: Contains STDP parameters including A_LTD, A_LTP, θ_LTD, θ_LTP, τu, τv, τx, Wmax, Wmin.
+    - `A_LTD`: Long Term Depression learning rate.
+    - `A_LTP`: Long Term Potentiation learning rate.
+    - `θ_LTD`: LTD threshold.
+    - `θ_LTP`: LTP threshold.
+    - `τu, τv, τx`: Time constants for different variables in STDP.
+    - `Wmax, Wmin`: Maximum and minimum synaptic weight.
+- `dt::Float32`: Time step for simulation.
+
+In addition to these, the function uses normalization where the operator can be multiplicative or additive as defined by `c.normalize.param.operator`.
+The `operator` is applied when updating the synaptic weights. The frequency of normalization is controlled by `τ`, 
+where if `τ > 0.0f0` then normalization will occur at intervals approximately equal to `τ`.
+
+After all updates, the synaptic weights are clamped between `Wmin` and `Wmax`.
+
+"""
 function plasticity!(c::SpikingSynapse, param::vSTDPParameter, dt::Float32)
     @unpack rowptr, colptr, I, J, index, W, W0, u, v, x, v_post, fireJ, t, g = c
     @unpack A_LTD, A_LTP, θ_LTD, θ_LTP, τu, τv, τx, Wmax, Wmin = param
-    R(x) = x < 0 ? eltype(x)(0) : x
+    R(x::Float32) = x < 0.f0 ? 0.f0 : x
 
 
     @inbounds @fastmath for j in eachindex(fireJ) # Iterate over all columns, j: presynaptic neuron
@@ -160,6 +200,18 @@ function plasticity!(c::SpikingSynapse, param::vSTDPParameter, dt::Float32)
 end
 
 
+"""
+    plasticity!(c::SynapseNormalization, param::AdditiveNorm, dt::Float32)
+
+Updates the synaptic weights using additive normalization. This function calculates 
+the rate of change `μ` as the difference between initial weight `W0` and the current weight `W1`, 
+normalized by `W1`. The weights are updated at intervals specified by time constant `τ`.
+
+# Arguments
+- `c`: An instance of SynapseNormalization.
+- `param`: An instance of AdditiveNorm.
+- `dt`: Simulation time step.
+"""
 function plasticity!(c::SynapseNormalization, param::AdditiveNorm, dt::Float32)
     @unpack W1, W0, μ, t = c
     @unpack τ, operator = param
@@ -171,6 +223,18 @@ function plasticity!(c::SynapseNormalization, param::AdditiveNorm, dt::Float32)
     end
 end
 
+"""
+    plasticity!(c::SynapseNormalization, param::MultiplicativeNorm, dt::Float32)
+
+Updates the synaptic weights using multiplicative normalization. This function calculates 
+the rate of change `μ` as the ratio of initial weight `W0` to the current weight `W1`.
+The weights are updated at intervals specified by time constant `τ`.
+
+# Arguments
+- `c`: An instance of SynapseNormalization.
+- `param`: An instance of MultiplicativeNorm.
+- `dt`: Simulation time step.
+"""
 function plasticity!(c::SynapseNormalization, param::MultiplicativeNorm, dt::Float32)
     @unpack W1, W0, μ, t = c
     @unpack τ, operator = param
