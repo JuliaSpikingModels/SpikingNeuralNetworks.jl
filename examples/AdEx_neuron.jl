dc9ec0e7a919efb523a9d27efe06e63ee68f68d6
Alessio <alessio.quaresima@mpi.nl>
2024-10-08 18:08:25 +0200

    added sim! for populations only

--- examples/AdEx_neuron.jl
+++ examples/AdEx_neuron.jl
@@ -11,7 +11,5 @@ E.param.b
 ##
 
 SNN.monitor(E, [:v, :fire, :w])
-SNN.sim!([E], []; duration = 700ms)
+SNN.sim!([E]; duration = 700ms)
 plot(SNN.vecplot(E, :w), SNN.vecplot(E, :v))
-
-E.param.C
--- src/main.jl
+++ src/main.jl
@@ -1,3 +1,22 @@
+"""
+    sim!(P::Vector{TN}, C::Vector{TS}, dt::Float32) where {TN <: AbstractNeuron, TS<:AbstractSynapse }
+
+Simulates the spiking neural network by iterating over the populations and synapses in the network and updating their states.
+
+**Arguments**
+- `P::Vector{TN}`: Vector of neurons in the network.
+- `C::Vector{TS}`: Vector of synapses in the network.
+- `dt::Float32`: Time step for the simulation.
+
+**Details**
+- The function then calls the `integrate!` function on `p` with its parameters and the time step `dt`.
+- Finally, the function calls the `record!` function on `p` to record its state.
+
+- For each synapse `c` in `C`, the function checks if `c` has a field `t` and increments its value by 1 if it exists.
+- The function then calls the `forward!` function on `c` with its parameters.
+- Finally, the function calls the `record!` function on `c` to record its state.
+
+"""
 function sim!(P::Vector{TN}, C::Vector{TS}, dt::Float32) where {TN <: AbstractNeuron, TS<:AbstractSynapse }
     # Threads.@threads 
     for p in P
@@ -13,9 +32,33 @@ function sim!(P::Vector{TN}, C::Vector{TS}, dt::Float32) where {TN <: AbstractNe
     end
 end
 
+"""
+    sim!(
+        P::Vector{TN},
+        C::Vector{TS};
+        dt = 0.1f0,
+        duration = 10.0f0,
+        pbar = false,
+    ) where {TN <: AbstractNeuron, TS<:AbstractSynapse }
+
+Simulates the spiking neural network for a specified duration by repeatedly calling `sim!` function.
+
+**Arguments**
+- `P::Vector{TN}`: Vector of neurons in the network.
+- `C::Vector{TS}`: Vector of synapses in the network.
+- `dt::Float32`: Time step for the simulation. Default value is `0.1f0`.
+- `duration::Float32`: Duration of the simulation. Default value is `10.0f0`.
+- `pbar::Bool`: Flag indicating whether to display a progress bar during the simulation. Default value is `false`.
+
+**Details**
+- The function creates a range of time steps from `0.0f0` to `duration-dt` with a step size of `dt`.
+- If `pbar` is `true`, the function creates a progress bar using the `ProgressBar` function with the time step range. Otherwise, it uses the time step range directly.
+- The function iterates over the time steps and calls the `sim!` function with `P`, `C`, and `dt`.
+
+"""
 function sim!(
     P::Vector{TN},
-    C::Vector{TS};
+    C::Vector{TS} = [EmptySynapse()];
     dt = 0.1f0,
     duration = 10.0f0,
     pbar = false,
@@ -29,6 +72,29 @@ function sim!(
     end
 end
 
+
+"""
+    train!(P::Vector{TN}, C::Vector{TS}, dt::Float32) where {TN <: AbstractNeuron, TS<:AbstractSynapse }
+
+Trains the spiking neural network by iterating over the neurons and synapses in the network and updating their states.
+
+**Arguments**
+- `P::Vector{TN}`: Vector of neurons in the network.
+- `C::Vector{TS}`: Vector of synapses in the network.
+- `dt::Float32`: Time step for the training.
+
+**Details**
+- For each neuron `p` in `P`, the function checks if `p` has a field `t` and increments its value by 1 if it exists.
+- The function then calls the `integrate!` function on `p` with its parameters and the time step `dt`.
+- Finally, the function calls the `record!` function on `p` to record its state.
+
+- For each synapse `c` in `C`, the function checks if `c` has a field `t` and increments its value by 1 if it exists.
+- The function then calls the `forward!` function on `c` with its parameters.
+- If `c` has a field `t` and its second element is approximately equal to 0, the function continues to the next iteration.
+- Otherwise, the function calls the `plasticity!` function on `c` with its parameters and the time step `dt`.
+- Finally, the function calls the `record!` function on `c` to record its state.
+
+"""
 function train!(P::Vector{TN}, C::Vector{TS}, dt::Float32) where {TN <: AbstractNeuron, TS<:AbstractSynapse }
     for p in P
         hasfield(typeof(p), :t) && (p.t[1] += 1)
@@ -45,6 +111,28 @@ function train!(P::Vector{TN}, C::Vector{TS}, dt::Float32) where {TN <: Abstract
     end
 end
 
+"""
+    train!(
+        P::Vector{TN},
+        C::Vector{TS};
+        dt = 0.1ms,
+        duration = 10ms,
+    ) where {TN <: AbstractNeuron, TS<:AbstractSynapse }
+
+Trains the spiking neural network for a specified duration by repeatedly calling `train!` function.
+
+**Arguments**
+- `P::Vector{TN}`: Vector of neurons in the network.
+- `C::Vector{TS}`: Vector of synapses in the network.
+- `dt::Float32`: Time step for the training. Default value is `0.1ms`.
+- `duration::Float32`: Duration of the training. Default value is `10ms`.
+
+**Details**
+- The function converts `dt` to `Float32` if it is not already.
+- The function creates a progress bar using the `ProgressBar` function with a range of time steps from `0.0f0` to `duration-dt` with a step size of `dt`.
+- The function iterates over the time steps and calls the `train!` function with `P`, `C`, and `dt`.
+
+"""
 function train!(
     P::Vector{TN},
     C::Vector{TS};
